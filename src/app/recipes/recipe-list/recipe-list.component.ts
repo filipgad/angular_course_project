af5e459dcb5125f014cf7d93b0999524a9be4f06
Filip Godziński <godzinski.filip@gmail.com>
2018-03-26 21:27:22 +0200

    Services and Dependency Injection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
     selector: 'app-recipe-list',
@@ -9,20 +10,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[];
 
-    recipes: Recipe[] = [
-        new Recipe('First Test Recipe', 'A Simply Test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Second Test Recipe', 'A Simply Test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Third Test Recipe', 'A Simply Test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-    ];
-
-    constructor() { }
+    constructor(private recipeService: RecipeService) { }
 
     ngOnInit() {
-    }
-
-    onRecipeSelected(recipe: Recipe) {
-        this.recipeWasSelected.emit(recipe);
+        this.recipes = this.recipeService.getRecipes();
     }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,52 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+
+export class RecipeService {
+
+    recipeSelected = new EventEmitter<Recipe>();
+
+    recipes: Recipe[] = [
+        new Recipe(
+            'Tasty Schnitzel', 
+            'What do you need?', 
+            'https://cdn.doradcasmaku.pl/dynamic/f3/3e/c1/82/dbe69de0bedaa9915c020ea5/z-g_fit-800-600.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Frech Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big burger', 
+            'What do you need?', 
+            'http://www.readersdigest.ca/wp-content/uploads/2015/11/gourmet-burger.jpg',
+            [
+                new Ingredient('Bread', 1),
+                new Ingredient('Meat', 1)
+            ]
+        ),
+        new Recipe(
+            'Spaghetti', 
+            'What do you need?', 
+            'https://cdn.aniastarmach.pl/content/uploads/2016/02/makaron-spaghetti-bolognese-2-1200x630.jpg',
+            [
+                new Ingredient('Pasta', 1),
+                new Ingredient('Tomates', 20)
+            ]
+        )
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
